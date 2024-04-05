fc127453820d1cc25cfbb67200eaa5c0ed6aff64
deftwnes <fabia.fr78@gmail.com>
2024-04-05 13:29:47 -0300

    fim

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const modalView = document.querySelector('#modal-view'); //Modal para exibição
 const notes = document.querySelector('#notes');//Lista divs com dados das notas
 const btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
 const btnCloseNote = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
-
+const divControls = document.querySelector("#controls-note");//editar e excluir notas
 
 //Eventos
 
@@ -18,15 +18,27 @@ addNote.addEventListener("click", (evt) =>{
     addNote.style.display = "none";
 });
 
+closeModal.addEventListener("click",(evt) => {
+    evt.preventDefault();
+    modalView.style.display = "none";
+    notes.style.display = "flex";
+    addNote.style.display = "block";
+
+    document.querySelector('#title-note').innerText = '';
+    document.querySelector('#content-note').innerText = '';
+})
+
 btnCloseNote.addEventListener("click", (evt) => {
     evt.preventDefault();
     modal.style.display = "none";
     notes.style.display = "flex";
     addNote.style.display = "block";
-    document.querySelector('#input-id').value = '';
-    document.querySelector('#input-title').value = '';
-    document.querySelector('#input-content').value = '';
-    listNotes();
+    
+    document.querySelector("#input-id").value = '';
+    document.querySelector("#input-title").value = '';
+    document.querySelector("#input-content").value = '';
+
+    listNotes(); 
 });
 
 btnSaveNote.addEventListener("click", (evt)=> {
@@ -37,20 +49,12 @@ btnSaveNote.addEventListener("click", (evt)=> {
         title: document.querySelector("#input-title").value,
         content: document.querySelector("#input-content").value,
         lastTime: new Date().getTime(),
-        
     }
-    
+
     saveNote(data);
-});
 
-closeModal.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    modalView.style.display="none";
-    notes.style.display='flex';
-    addNote.style.display='block';
-    document.querySelector('#title-note').innerText = '';
-    document.querySelector('#content-note').innerText='';
 
+    
 });
 
 
@@ -67,11 +71,10 @@ const saveNote = (data) => {
     } else {
         console.log(data.id);
         notes.forEach((item, i) => {
-            if(item.id===data.id){
-                console.log("*");
+            if(item.id == data.id) {
                 notes[i] = data;
             }
-        })
+        });
     }
 
     console.log(data);
@@ -134,6 +137,7 @@ const showNote = (item) => {
     notes.style.display = "none";
     addNote.style.display = "none";
     modalView.style.display = "block";
+    divControls.style.display = "flex";
 
     document.querySelector('#title-note').innerText = item.title;
 
@@ -145,13 +149,72 @@ const showNote = (item) => {
     pLastTime.innerText = "Última alteração: " + new Date(item.lastTime).toLocaleDateString('pt-br');
     document.querySelector('#content-note').appendChild(pLastTime);
 
-    let contrNote = document.querySelector("#controls-note");
-    let aDoContr = document.createElement('a');
-
-    aDoContr.appendChild(contrNote);
-    contrNote.appendChild(modalView);
-
+    divControls.innerHTML = '';
     
+    divEdit = document.createElement('div');
+    linkEdit = document.createElement('a');
+    linkEdit.setAttribute("id", item.id);
+    iconEdit = document.createElement('i');
+    iconEdit.className = "bi bi-pen";
+    iconEdit.style.color = "blue";
+    linkEdit.appendChild(iconEdit);
+    divEdit.appendChild(linkEdit);
+    divControls.appendChild(divEdit);
+
+    divDel = document.createElement('div');
+    linkDel = document.createElement('a');
+    linkDel.setAttribute("id", item.id);
+    iconDel = document.createElement('i');
+    iconDel.className = "bi bi-trash";
+    iconDel.style.color = "red";
+    linkDel.appendChild(iconDel);
+    divDel.appendChild(linkDel);
+    divControls.appendChild(divDel);
+
+
+    linkDel.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        if (confirm("Confirmar?")) {
+            deleteNote (item.id);
+        }
+    });
+
+    linkEdit.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        modal.style.display = "block"; 
+        notes.style.display = "none";
+        addNote.style.display = "none";
+        modalView.style.display = "none";
+        divControls.style.display = "none";
+        editNote(item.id);
+    });
+};
+
+const deleteNote = (id) => {
+    console.log(id);
+    let allNotes = loadNotes(); 
+
+    let filteredNotes = allNotes.filter(note => note.id !== id);
+    localStorage.setItem('notes', JSON.stringify(filteredNotes));
+
+    listNotes();
+};
+
+const editNote = (id) => {
+    console.log(id);
+    let notes = loadNotes();
+    const noteToEdit = notes.find(note => note.id === id);
+
+    if (noteToEdit) {
+        document.querySelector("#input-id").value = noteToEdit.id;
+        document.querySelector("#input-title").value = noteToEdit.title;
+        document.querySelector("#input-content").value = noteToEdit.content;
+    } else {
+        console.log("nota não encontrada");
+    }
 };
 
 listNotes();
+
+
+//METODO trim() TIRA OS ESPAÇOS DAS STRINGS
